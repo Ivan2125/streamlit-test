355d62417528e8c6cd0b21b70827b0e05e7d5a18
Ivan2125 <cristianparra2125@gmail.com>
2024-04-17 22:37:56 -0500

    Update Tableau visualization and add new sections in streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -220,7 +220,7 @@ if tabs == "Home":
 elif tabs == "Dashboard":
 
     st.header("Dashboard \U0001F4CA", divider="rainbow", anchor=False)
-    st.header("📊 1. Tiendas de conveniencia | Panorama general:")
+    st.header("💰 1. Tiendas de conveniencia | Panorama general:")
     st.subheader(
         "➡️ Este tablero ofrece una vista general del panorama del mercado de tiendas de conveniencia, con un enfoque especial en el cliente Walgreens."
     )
@@ -279,11 +279,11 @@ elif tabs == "Dashboard":
         "➡️ Presenta los indicadores clave de rendimiento (KPIs) de Walgreens en el mercado de tiendas de conveniencia, comparándolos con la competencia directa."
     )
 
-    embed_code = """
+    embed_code2 = """
                 <div style='display: flex; justify-content: center; width: 100%; margin: auto'>
             <div class='tableauPlaceholder' id='viz1713410941026' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Wa&#47;Walgreens-marketanalysis-GoogleYelp&#47;2_KPIs&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Walgreens-marketanalysis-GoogleYelp&#47;2_KPIs' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Wa&#47;Walgreens-marketanalysis-GoogleYelp&#47;2_KPIs&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='es-ES' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1713410941026');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.minHeight='1000px';vizElement.style.maxHeight=(divElement.offsetWidth*1.77)+'px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
     components.html(
-        embed_code,
+        embed_code2,
         height=2000,
         width=1709,
     )
